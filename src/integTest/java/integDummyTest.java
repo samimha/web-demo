c1fa90f27b41da52a10aa3a993368c5c3b8cc959
Sami Haapanen <sami.haapanen@hotmail.com>
2018-04-17 12:00:47 +0300

    added integtest

---
+++ src/integTest/java/integDummyTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sami
+ */
+public class integDummyTest {
+    
+    @Test
+    public void dummyUnitTest() {
+        assertEquals(1, 1);
+    }
+
+}
